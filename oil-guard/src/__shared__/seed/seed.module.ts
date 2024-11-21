55376f286fb38ba125310ad2565840e20be2162c
iradukundapaci <paifique57@gmail.com>
2024-11-21 16:39:42 +0200

    adding seed

--- oil-guard/src/__shared__/seed/seed.module.ts
+++ oil-guard/src/__shared__/seed/seed.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { AdminSeedService } from "./admin-seed.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "src/users/entities/user.entity";
+import { Pipeline } from "src/pipeline/entities/pipeline.entity";
+import { Sensor } from "src/sensor/entities/sensor.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Pipeline, Sensor])],
   providers: [AdminSeedService],
   exports: [AdminSeedService],
 })
